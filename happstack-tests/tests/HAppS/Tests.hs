3e64668caad742e5667aeae9d0ff4d6016677991
jeremy <jeremy@n-heptane.com>
2009-02-02 22:19:54 +0000

    testsuite: incorporated IxSet. Adds 1 new test.

--- happstack-tests/tests/HAppS/Tests.hs
+++ happstack-tests/tests/HAppS/Tests.hs
@@ -2,7 +2,8 @@ module HAppS.Tests where
 
 import qualified HAppS.Util.Tests as Util
 import qualified HAppS.Data.Tests as Data
+import qualified HAppS.Data.IxSet.Tests as IxSet
 import Test.HUnit
 
 allTests :: Test
-allTests = ("happstack" ~: [ Util.allTests, Data.allTests ])
+allTests = ("happstack" ~: [ Util.allTests, Data.allTests, IxSet.allTests ])
