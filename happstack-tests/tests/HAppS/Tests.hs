6aaea0efbd76b24eec6cd280d7b06a9b1b5a3721
jeremy <jeremy@n-heptane.com>
2009-02-03 16:37:41 +0000

    testsuite: add happs-server and happs-contrib

--- happstack-tests/tests/HAppS/Tests.hs
+++ happstack-tests/tests/HAppS/Tests.hs
@@ -4,7 +4,9 @@ import qualified HAppS.Util.Tests       as Util
 import qualified HAppS.Data.Tests       as Data
 import qualified HAppS.Data.IxSet.Tests as IxSet
 import qualified HAppS.State.Tests      as State
+import qualified HAppS.Server.Tests     as Server
+import qualified HAppS.Server.Tests     as Contrib
 import Test.HUnit
 
 allTests :: Test
-allTests = ("happstack" ~: [ Util.allTests, Data.allTests, IxSet.allTests, State.allTests ])
+allTests = ("happstack" ~: [ Util.allTests, Data.allTests, IxSet.allTests, State.allTests, Server.allTests, Contrib.allTests ])
