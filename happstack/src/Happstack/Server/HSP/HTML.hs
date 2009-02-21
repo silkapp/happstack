286770fc1ee606157e4f2dd71f46e2723fc590be
Matthew Elder <matt@mattelder.org>
2009-02-21 23:00:17 +0000

    cleaned up imports in Happstack.Server.HSP.HTML & fixed compiler warning

--- happstack/src/Happstack/Server/HSP/HTML.hs
+++ happstack/src/Happstack/Server/HSP/HTML.hs
@@ -1,16 +1,24 @@
 {-# LANGUAGE FlexibleInstances, FlexibleContexts #-}
-module Happstack.Server.HSP.HTML 
-    ( webHSP
-    )
-    where
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+module Happstack.Server.HSP.HTML
+  ( webHSP
+  ) where
 
-import Control.Monad.Trans(MonadIO, liftIO)
+import Control.Monad.Trans (MonadIO(), liftIO)
 import qualified Data.ByteString.Char8 as P
 import qualified Data.ByteString.Lazy.UTF8 as L
-import Happstack.Server(ToMessage(toMessage, toContentType,toResponse),Response)
-import HSP (HSP, evalHSP)
-import HSP.XML (XML(..), XMLMetaData(..))
-import HSP.HTML (html4Strict, renderAsHTML)
+import Happstack.Server
+  ( ToMessage(toMessage, toContentType, toResponse)
+  , Response()
+  )
+import HSP
+  ( HSP()
+  , XML()
+  , XMLMetaData(XMLMetaData, contentType)
+  , evalHSP
+  , html4Strict
+  , renderAsHTML
+  )
 
 instance ToMessage XML where
     toContentType _ = P.pack "text/html;charset=utf-8"
@@ -26,3 +34,4 @@ instance ToMessage (Maybe XMLMetaData, XML) where
 
 webHSP :: (MonadIO m) => HSP XML -> m Response
 webHSP hsp = return . toResponse =<< liftIO (evalHSP Nothing hsp)
+
