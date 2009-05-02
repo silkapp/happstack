e26b3124546913e49221bd3bd15bc484b7de8515
Matthew Elder <matt@mattelder.org>
2009-05-02 21:42:30 +0000

    whoops forgot an important file

---
+++ happstack-util/src/Happstack/Util/OpenExclusively.hs
@@ -0,0 +1,24 @@
+ -- | Cross platform way to open a file exclusively
+module Happstack.Util.OpenExclusively
+    ( -- | Cross platform way to open a file exclusively
+      openExclusively
+    ) where
+    
+import System.IO
+
+#ifdef mingw32_HOST_OS
+-- Windows opens files for exclusive writing by default
+openExclusively :: FilePath -> IO Handle
+openExclusively fp = openFile fp ReadWriteMode
+#endif
+
+#ifndef mingw32_HOST_OS
+import System.Posix.IO
+
+-- Unix needs to use a special open call to open files for exclusive writing
+openExclusively :: FilePath -> IO Handle
+openExclusively fp =
+    fdToHandle =<< openFd fp ReadWrite (Just 0o600) flags
+    where flags = defaultFileFlags {exclusive = True, trunc = True}
+#endif
+
