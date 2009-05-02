6323a851f3297a11ab37c002189ea69243a74fe6
Matthew Elder <matt@mattelder.org>
2009-05-02 22:06:46 +0000

    removed contrib from -all scripts

--- bin/build-install-all.sh
+++ bin/build-install-all.sh
@@ -5,6 +5,5 @@ cd ../happstack-data    && cabal install "$@" && \
 cd ../happstack-state   && cabal install "$@" && \
 cd ../happstack-ixset   && cabal install "$@" && \
 cd ../happstack-server  && cabal install "$@" && \
-cd ../happstack-contrib && cabal install "$@" && \
 cd ../happstack         && cabal install "$@" && \
 cd .. && echo "Done"
