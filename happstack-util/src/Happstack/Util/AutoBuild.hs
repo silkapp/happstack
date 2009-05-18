037904c6dbd36cfcdb6757d60c190e6379e75ece
Matthew Elder <matt@mattelder.org>
2009-05-18 07:34:43 +0000

    overhauled build auto functionality, should be more robust now as an example using the guestbook app, you can run these commands:   happstack new project buildtestproject   cd buildtestproject   cabal configure   happstack build auto "cabal build" dist/build/guestbook-server/guestbook-server new builds will be detected automatically

--- happstack-util/src/Happstack/Util/AutoBuild.hs
+++ happstack-util/src/Happstack/Util/AutoBuild.hs
@@ -13,17 +13,14 @@ module Happstack.Util.AutoBuild (
     autoBuild
     ) where
 
-import Control.Timeout (addTimeout)
-import Data.Maybe (isNothing)
+import Control.Concurrent (forkIO, threadDelay)
+import Control.Concurrent.MVar (MVar, newEmptyMVar, putMVar, takeMVar)
+import Control.Exception (bracket)
 import System.Directory (getModificationTime)
-import System.Exit (ExitCode(..))
+import System.Exit (ExitCode(..), exitFailure)
 import System.Process
 import System.Time (ClockTime)
 import System.IO
-
-log' :: [Char] -> IO ()
-log' ""  = return ()
-log' msg = appendFile "build.log" (msg ++ "\n")
 
 -- | Functionality for the autoBuild tool.
 --   Inspired by searchpath.
@@ -32,73 +29,65 @@ autoBuild :: String   -- ^ Build command
           -> [String] -- ^ Arguments to use when running binary
           -> IO ()
 autoBuild buildCmd binPath binArgs = do
-    log' "Initial Build . . ."
-    buildBin buildCmd
-    spawnLoop buildCmd binPath binArgs
-    return ()
-
-{- flow control -}
-
--- spawn the binary over and over again
-spawnLoop :: String -> String -> [String] -> IO ()
-spawnLoop buildCmd binPath binArgs = do
-    lastMod <- getModificationTime binPath
-    ph <- runBin binPath binArgs
-
-    -- get the build/kill loop going
-    buildKillLoop binPath buildCmd ph lastMod
-
-    -- will wait for natural death or death by buildKillLoop
-    waitForProcess ph
- 
-    -- do it all over again
-    spawnLoop buildCmd binPath binArgs
-
--- monitor file modification time, keep trying to build and kill the ph once there is a new version
-buildKillLoop :: String -> String -> ProcessHandle -> ClockTime -> IO ()
-buildKillLoop binPath buildCmd ph lastMod = do
-    isActive <- fmap isNothing (getProcessExitCode ph)
-    -- is the process still active?
-    if isActive
+    putStrLn "Performing the initial build. . ."
+    buildSuccess <- buildBin buildCmd -- initial build
+    if buildSuccess
         then do
-            buildSuccess <- buildBin buildCmd
+            mph <- newEmptyMVar
             newMod <- getModificationTime binPath
+            forkIO (builder mph buildCmd binPath newMod)
+            runner mph binPath binArgs
+            
+        else do
+            putStrLn "Initial build failed, see 'build.out.log' and 'build.err.log'."
+            exitFailure
 
-            -- was the build successful? is there a new binary?
-            if buildSuccess && (newMod /= lastMod)
-                then do
-                    log' "New Build Detected . . ."
-                    killBin ph
-                else do
-                    -- wait 5 seconds before repeating
-                    addTimeout 5 $ buildKillLoop binPath buildCmd ph newMod
-                    return ()
+-- builds the program
+builder :: MVar ProcessHandle -> String -> FilePath -> ClockTime -> IO ()
+builder mph buildCmd binPath lastMod = do
+    -- add a delay between build attempts (5 seconds)
+    threadDelay 5000000
+    
+    buildSuccess <- buildBin buildCmd
+    newMod <- getModificationTime binPath
+    
+    -- if the build yielded a new binary, terminate the process
+    if buildSuccess && (newMod /= lastMod)
+        then do
+            putStrLn "A new binary has been built, killing the existing one. . ."
+            terminateProcess =<< takeMVar mph
         else return ()
+        
+    -- continue loop
+    builder mph buildCmd binPath newMod
 
-{- meat and potatoes -}
+-- runs the program
+runner :: MVar ProcessHandle -> FilePath -> [String] -> IO ()
+runner mph binPath binArgs = do
+    bracket
+        (runBin binPath binArgs)
+        (terminateProcess)
+        (\ph -> putMVar mph ph >> waitForProcess ph)
+    
+    -- continue loop
+    runner mph binPath binArgs
 
 -- does not block, returns ph
 runBin :: String -> [String] -> IO ProcessHandle
 runBin binPath binArgs = do
-    log' "Running . . ."
-    log' (showCmd binPath binArgs)
-    runProcess binPath binArgs Nothing Nothing (Just stdin) (Just stdout) (Just stderr)
+    putStrLn $ "Running binary: " ++ (showCmd binPath binArgs)
+    ph <- runProcess binPath binArgs Nothing Nothing Nothing Nothing Nothing
+    return ph
     where showCmd bp [] = bp
           showCmd bp ba = bp ++ " " ++ unwords ba
 
 -- blocks until built, returns True if build was a success
 buildBin :: String -> IO Bool
 buildBin buildCmd = do
-    (_inp,_out,err,ph) <- runInteractiveCommand buildCmd
-    log' =<< hGetContents err
+    (_inp,out,err,ph) <- runInteractiveCommand buildCmd
+    appendFile "build.out.log" =<< hGetContents out
+    appendFile "build.err.log" =<< hGetContents err
     waitForProcess ph
     exitCode <- getProcessExitCode ph
     return (exitCode == Just ExitSuccess)
-
--- does not block, kills process
-killBin :: ProcessHandle -> IO ()
-killBin ph = do
-    log' "Killing . . ."
-    terminateProcess ph
-    return ()
 
