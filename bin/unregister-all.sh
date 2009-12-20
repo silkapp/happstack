359596b841a140b0dd9cb75dcfc70836ecfbfd2d
jeremy <jeremy@n-heptane.com>
2009-12-20 14:35:13 +0000

    added bin/unregister-all.sh

---
+++ bin/unregister-all.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+echo "unregistering all cabal packages" && \
+ghc-pkg unregister happstack --user
+ghc-pkg unregister happstack-server --user
+ghc-pkg unregister happstack-state --user
+ghc-pkg unregister happstack-ixset --user
+ghc-pkg unregister happstack-data --user
+ghc-pkg unregister happstack-util --user
+cd .. && echo "Done"
