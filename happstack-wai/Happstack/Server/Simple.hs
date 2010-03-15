98eb077ffdb92f4bb8739d238599c4f75e4e4e94
Antoine Latter <aslatter@gmail.com>
2010-03-15 05:03:35 +0000

    export conversion of a server part to a wai application

--- happstack-wai/Happstack/Server/Simple.hs
+++ happstack-wai/Happstack/Server/Simple.hs
@@ -1,25 +1,30 @@
-module Happstack.Server.Simple where
+module Happstack.Server.Simple
+    ( simpleHTTP
+    , application
+    ) where
 
 import Control.Monad (mzero)
 import qualified Data.ByteString.Lazy.Char8 as C
 import qualified Data.ByteString.Char8 as B
 import qualified Data.ByteString.Lazy.UTF8       as LU (toString, fromString)
 import Happstack.Server.Monads (Request(..), ServerPartT, runWebT, runServerPartT)
-import Network.Wai (Response, pathInfo)
+import Network.Wai (Application, Response, pathInfo)
 import qualified Network.Wai as Wai (Request(pathInfo))
 import Network.Wai.Handler.SimpleServer (run)
 import Web.Encodings (decodeUrl)
 import System.FilePath (splitDirectories)
 
+-- | Execute a serer part on the given port
 simpleHTTP :: Int -> ServerPartT IO Response -> IO ()
-simpleHTTP port sp = run port app
-  where
-    app :: Wai.Request -> IO Response
-    app rq = 
-      do mRes <- runWebT $ runServerPartT sp $ toRequest rq
-         case mRes of
-           Nothing -> mzero
-           (Just res) -> return res
+simpleHTTP port sp = run port $ application sp
+
+-- | Integrate a server part with the wai platform
+application :: ServerPartT IO Response -> Application
+application sp rq =
+    do mRes <- runWebT $ runServerPartT sp $ toRequest rq
+       case mRes of
+         Nothing -> mzero
+         (Just res) -> return res
 
 -- | Convert a WAI request to a Happstack request
 toRequest :: Wai.Request -> Request
