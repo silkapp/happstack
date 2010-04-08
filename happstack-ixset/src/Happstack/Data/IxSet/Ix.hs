3b1f6dba5b2152969f5c0c733db11f795e86d251
gracjanpolak <gracjanpolak@gmail.com>
2010-04-08 22:10:17 +0000

    Fix the Ix module

--- happstack-ixset/src/Happstack/Data/IxSet/Ix.hs
+++ happstack-ixset/src/Happstack/Data/IxSet/Ix.hs
@@ -2,26 +2,36 @@
              MultiParamTypeClasses, TemplateHaskell, PolymorphicComponents,
              DeriveDataTypeable,ExistentialQuantification #-}
 
-module Happstack.Data.IxSet.Ix where
+{- |
+
+This module defines typable indices and convenience functions. Should
+be probably considered private to 'Happstack.Data.IxSet'.
+
+-}
+module Happstack.Data.IxSet.Ix 
+    ( Ix(..)
+    , insert
+    , delete
+    ) 
+    where
 
 import Data.Generics hiding (GT)
 import           Data.Map (Map)
 import qualified Data.Map as Map
 import           Data.Set (Set)
 import qualified Data.Set as Set
-import Happstack.Data
 import qualified Data.Generics.SYB.WithClass.Basics as SYBWC
 
 -- the core datatypes
 
-data Ix a = IxDefault |
-            forall key . (Typeable key, Ord key) => Ix (Map key (Set a))
+-- | 'Ix' is a 'Map' from some 'Typeable' key to a set of values for
+-- that key.  'Ix' carries type information inside.
+data Ix a = forall key . (Typeable key, Ord key) => Ix (Map key (Set a))
     deriving Typeable
 
  -- minimal hacky instance
 instance Data a => Data (Ix a) where
     toConstr (Ix _) = con_Ix_Data
-    toConstr _ = error "unexpected match for: toConstr"
     gunfold _ _     = error "gunfold"
     dataTypeOf _    = ixType_Data
 
@@ -31,35 +41,29 @@ con_Ix_Data = mkConstr ixType_Data "Ix" [] Prefix
 ixType_Data :: DataType
 ixType_Data = mkDataType "Happstack.Data.IxSet.Ix" [con_Ix_Data]
 
-
-instance Default a => Default (Ix a) where
-    defaultValue = IxDefault
-
-ixDefaultConstr :: SYBWC.Constr
-ixDefaultConstr = SYBWC.mkConstr ixDataType "IxDefault" [] SYBWC.Prefix
 ixConstr :: SYBWC.Constr
 ixConstr = SYBWC.mkConstr ixDataType "Ix" [] SYBWC.Prefix
 ixDataType :: SYBWC.DataType
-ixDataType = SYBWC.mkDataType "Ix" [ixDefaultConstr, ixConstr]
+ixDataType = SYBWC.mkDataType "Ix" [ixConstr]
 
 instance (SYBWC.Data ctx a, SYBWC.Sat (ctx (Ix a)))
        => SYBWC.Data ctx (Ix a) where
     gfoldl = error "gfoldl Ix"
-    toConstr _ IxDefault = ixDefaultConstr
     toConstr _ (Ix _)    = ixConstr
     gunfold = error "gunfold Ix"
     dataTypeOf _ _ = ixDataType
 
 -- modification operations
 
--- | Convenience function for inserting into Maps of Sets
--- as in the case of an Ix.  If they key did not already exist in the Map, then a new Set is added transparently.
+-- | Convenience function for inserting into 'Map's of 'Set's as in
+-- the case of an 'Ix'.  If they key did not already exist in the
+-- 'Map', then a new 'Set' is added transparently.
 insert :: (Ord a, Ord k)
        => k -> a -> Map k (Set a) -> Map k (Set a)
 insert k v index = Map.insertWith Set.union k (Set.singleton v) index
 
--- | Convenience function for deleting from Maps of Sets
--- If the resulting Set is empty, then the entry is removed from the Map
+-- | Convenience function for deleting from 'Map's of 'Set's If the
+-- resulting 'Set' is empty, then the entry is removed from the 'Map'.
 delete :: (Ord a, Ord k)
        => k -> a -> Map k (Set a) -> Map k (Set a)
 delete k v index = Map.update remove k index
