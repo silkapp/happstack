ed2831c94d1438b16b498216d532df4bb18ae2ee
jeremy <jeremy@n-heptane.com>
2010-10-26 23:22:31 +0000

    happstack: add HasRqData instance for XMLGenT

--- happstack/src/Happstack/Server/HSX.hs
+++ happstack/src/Happstack/Server/HSX.hs
@@ -2,7 +2,7 @@
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 module Happstack.Server.HSX where
 
-import Happstack.Server.SimpleHTTP (ServerMonad(..), FilterMonad(..), WebMonad(..))
+import Happstack.Server.SimpleHTTP (ServerMonad(..), FilterMonad(..), WebMonad(..), HasRqData(..))
 import HSX.XMLGenerator (XMLGenT(..))
 
 instance (ServerMonad m) => ServerMonad (XMLGenT m) where
@@ -16,3 +16,8 @@ instance (FilterMonad a m) => FilterMonad a (XMLGenT m) where
 
 instance (WebMonad a m) => WebMonad a (XMLGenT m) where
     finishWith r = XMLGenT $ finishWith r
+
+instance (HasRqData m) => (HasRqData (XMLGenT m)) where
+    askRqEnv = XMLGenT askRqEnv
+    localRqEnv f (XMLGenT m) = XMLGenT (localRqEnv f m)
+    rqDataError = XMLGenT . rqDataError
