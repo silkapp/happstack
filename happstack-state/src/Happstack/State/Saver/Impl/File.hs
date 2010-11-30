44123d7c0a999346eb2d66581edc98be59f5f401
David Fox <dsf@seereason.com>
2010-11-30 19:59:37 +0000

    Add a Unix-only version of obtainLock which records the pid and breaks the lock if it can

--- happstack-state/src/Happstack/State/Saver/Impl/File.hs
+++ happstack-state/src/Happstack/State/Saver/Impl/File.hs
@@ -21,9 +21,22 @@ import System.Log.Logger
 import Text.Printf
 import Control.Monad
 import System.FilePath
+#ifdef UNIX
+import Data.Maybe (listToMaybe)
+import qualified System.IO.Error as SE
+import System.Posix.IO (openFd, OpenMode(ReadWrite), defaultFileFlags, exclusive, trunc, fdToHandle)
+import System.Posix.Process (getProcessID)
+import System.Posix.Signals (nullSignal, signalProcess)
+import System.Posix.Types (ProcessID)
+#else
 import Happstack.Util.OpenExclusively (openExclusively)
+#endif
 
+#ifdef UNIX
+newtype PrefixLock = PrefixLock FilePath
+#else
 type PrefixLock = (FilePath, Handle)
+#endif
 
 tryE :: IO a -> IO (Either SomeException a)
 tryE = E.try
@@ -31,8 +44,10 @@ tryE = E.try
 catchE :: IO a -> (SomeException -> IO a) -> IO a
 catchE = E.catch
 
+#ifndef UNIX
 catchIO :: IO a -> (IOException -> IO a) -> IO a
 catchIO = E.catch
+#endif
 
 logMF :: Priority -> String -> IO ()
 logMF = logM "Happstack.State.Saver.Impl.File"
@@ -106,6 +121,103 @@ atomicWriteFile path string = do
   L.writeFile p' string
   renameFile p' path
 
+#ifdef UNIX
+obtainPrefixLock :: FilePath -> IO PrefixLock
+obtainPrefixLock prefix = do
+    checkLock fp >> takeLock fp
+    where fp = prefix ++ ".lock"
+
+-- |Read the lock and break it if the process is dead.
+checkLock :: FilePath -> IO ()
+checkLock fp = readLock fp >>= maybeBreakLock fp
+
+-- |Read the lock and return the process id if possible.
+readLock :: FilePath -> IO (Maybe ProcessID)
+readLock fp = try (readFile fp) >>=
+              return . either (checkReadFileError fp) (fmap (fromInteger . read) . listToMaybe . lines)
+
+-- |Is this a permission error?  If so we don't have permission to
+-- remove the lock file, abort.
+checkReadFileError :: [Char] -> IOError -> Maybe ProcessID
+checkReadFileError fp e | SE.isPermissionError e = throw (userError ("Could not read lock file: " ++ show fp))
+                        | SE.isDoesNotExistError e = Nothing
+                        | True = throw e
+
+maybeBreakLock :: FilePath -> Maybe ProcessID -> IO ()
+maybeBreakLock fp Nothing =
+    -- The lock file exists, but there's no PID in it.  At this point,
+    -- we will break the lock, because the other process either died
+    -- or will give up when it failed to read its pid back from this
+    -- file.
+    breakLock fp
+maybeBreakLock fp (Just pid) = do
+  -- The lock file exists and there is a PID in it.  We can break the
+  -- lock if that process has died.
+  -- getProcessStatus only works on the children of the calling process.
+  -- exists <- try (getProcessStatus False True pid) >>= either checkException (return . isJust)
+  exists <- doesProcessExist pid
+  case exists of
+    True -> throw (lockedBy fp pid)
+    False -> breakLock fp
+
+doesProcessExist :: ProcessID -> IO Bool
+doesProcessExist pid =
+    -- Implementation 1
+    -- doesDirectoryExist ("/proc/" ++ show pid)
+    -- Implementation 2
+    try (signalProcess nullSignal pid) >>= return . either checkException (const True)
+    where checkException e | SE.isDoesNotExistError e = False
+                           | True = throw e
+
+-- |We have determined the locking process is gone, try to remove the
+-- lock.
+breakLock :: FilePath -> IO ()
+breakLock fp = try (removeFile fp) >>= either checkBreakError (const (return ()))
+
+-- |An exception when we tried to break a lock, if it says the lock
+-- file has already disappeared we are still good to go.
+checkBreakError :: IOError -> IO ()
+checkBreakError e | SE.isDoesNotExistError e = return ()
+                  | True = throw e
+
+-- |Try to create lock by opening the file with the O_EXCL flag and
+-- writing our PID into it.  Verify by reading the pid back out and
+-- matching, maybe some other process slipped in before we were done
+-- and broke our lock.
+takeLock :: FilePath -> IO PrefixLock
+takeLock fp = do
+  createDirectoryIfMissing True (takeDirectory fp) 
+  h <- openFd fp ReadWrite (Just 0o600) (defaultFileFlags {exclusive = True, trunc = True}) >>= fdToHandle
+  pid <- getProcessID
+  hPutStrLn h (show pid) >> hClose h
+  -- Read back our own lock and make sure its still ours
+  readLock fp >>= maybe (throw (cantLock fp pid))
+                        (\ pid' -> if pid /= pid'
+                                   then throw (stolenLock fp pid pid')
+                                   else return (PrefixLock fp))
+
+-- |An exception saying the data is locked by another process.
+lockedBy :: (Show a) => FilePath -> a -> SomeException
+lockedBy fp pid = SomeException (SE.mkIOError SE.alreadyInUseErrorType ("Locked by " ++ show pid) Nothing (Just fp))
+
+-- |An exception saying we don't have permission to create lock.
+cantLock :: FilePath -> ProcessID -> SomeException
+cantLock fp pid = SomeException (SE.mkIOError SE.alreadyInUseErrorType ("Process " ++ show pid ++ " could not create a lock") Nothing (Just fp))
+
+-- |An exception saying another process broke our lock before we
+-- finished creating it.
+stolenLock :: FilePath -> ProcessID -> ProcessID -> SomeException
+stolenLock fp pid pid' = SomeException (SE.mkIOError SE.alreadyInUseErrorType ("Process " ++ show pid ++ "'s lock was stolen by process " ++ show pid') Nothing (Just fp))
+
+-- |Relinquish the lock by removing it and then verifying the removal.
+releasePrefixLock :: PrefixLock -> IO ()
+releasePrefixLock (PrefixLock fp) =
+    dropLock >>= either checkDrop return
+    where
+      dropLock = try (removeFile fp)
+      checkDrop e | SE.isDoesNotExistError e = return ()
+                  | True = throw e
+#else
 obtainPrefixLock :: FilePath -> IO PrefixLock
 obtainPrefixLock prefix = do
     createDirectoryIfMissing True prefix
@@ -125,5 +237,4 @@ releasePrefixLock (fp, h) = do
      tryE $ hClose h
      tryE $ removeFile fp
      return ()
-
-
+#endif
