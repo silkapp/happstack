538a06f5a44dc9b82310a6b253ab53cf63b81110
jeremy <jeremy@n-heptane.com>
2011-02-01 21:11:13 +0000

    added packdeps-all.sh script to bin/

---
+++ bin/packdeps-all.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+for package in happstack-util happstack-data happstack-ixset happstack-state happstack-server happstack happstack-hamlet happstack-heist happstack-hsp happstack-hstringtemplate happstack-plugins
+do
+  packdeps $package/$package.cabal
+done
+
