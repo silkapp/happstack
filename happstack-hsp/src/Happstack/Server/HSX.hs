dce0a6864434bf1d27f908ffb14a93ccaa99b7bb
jeremy <jeremy@n-heptane.com>
2011-02-01 21:39:27 +0000

    happstack-hsp: Added Happstack instance for XMLGenT. And some haddock documentation to Happstack.Server.HSX

--- happstack-hsp/src/Happstack/Server/HSX.hs
+++ happstack-hsp/src/Happstack/Server/HSX.hs
@@ -1,9 +1,12 @@
-{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses, UndecidableInstances #-}
+-- | This module contains orphan 'XMLGenT' instances for 'ServerMonad', 'FilterMonad', 'WebMonad', 'HasRqData', and 'Happstack'. It does not export any functions.
+{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses, TypeFamilies, UndecidableInstances #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 module Happstack.Server.HSX where
 
-import Happstack.Server.SimpleHTTP (ServerMonad(..), FilterMonad(..), WebMonad(..), HasRqData(..))
-import HSX.XMLGenerator (XMLGenT(..))
+import Control.Monad               (MonadPlus(..))
+import Control.Monad.Trans         (MonadIO(..))
+import Happstack.Server.SimpleHTTP (ServerMonad(..), FilterMonad(..), WebMonad(..), HasRqData(..), Happstack(..), Response)
+import HSX.XMLGenerator            (XMLGenT(..))
 
 instance (ServerMonad m) => ServerMonad (XMLGenT m) where
     askRq = XMLGenT askRq
@@ -21,3 +24,5 @@ instance (HasRqData m) => (HasRqData (XMLGenT m)) where
     askRqEnv = XMLGenT askRqEnv
     localRqEnv f (XMLGenT m) = XMLGenT (localRqEnv f m)
     rqDataError = XMLGenT . rqDataError
+
+instance (MonadPlus m, Functor m, MonadIO m, ServerMonad m, FilterMonad a m, WebMonad a m, HasRqData m, a ~ Response) => Happstack (XMLGenT m)
