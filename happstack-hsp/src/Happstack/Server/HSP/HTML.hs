29c8fd69725073a24a4759ce39564204214b7bc8
jeremy <jeremy@n-heptane.com>
2011-02-21 21:19:00 +0000

    happstack-hsp: bumped to 6.0.1. Added defaultTemplate, re-export HSP and HSP.ServerPart() from Happstack.Server.HSP.Html.

--- happstack-hsp/src/Happstack/Server/HSP/HTML.hs
+++ happstack-hsp/src/Happstack/Server/HSP/HTML.hs
@@ -1,8 +1,11 @@
+-- | support for using HSP+Happstack for rendering HTML 
 {-# LANGUAGE FlexibleInstances, FlexibleContexts #-}
-{-# OPTIONS_GHC -fno-warn-orphans #-}
+{-# OPTIONS_GHC -fno-warn-orphans -F -pgmFtrhsx #-}
 module Happstack.Server.HSP.HTML
-  ( webHSP
+  ( defaultTemplate
+  , webHSP
   , webHSP'
+  , module HSP
   ) where
 
 import Control.Monad.Trans (MonadIO(), liftIO)
@@ -11,16 +14,11 @@ import qualified Data.ByteString.Lazy.UTF8 as L
 import Control.Monad (liftM)
 import Happstack.Server
   ( ToMessage(toMessage, toContentType, toResponse)
-  , Response()
+  , Response
   )
 import HSP
-  ( HSP()
-  , XML()
-  , XMLMetaData(XMLMetaData, contentType)
-  , evalHSP
-  , html4Strict
-  , renderAsHTML
-  )
+import HSP.ServerPartT ()
+import qualified HSX.XMLGenerator as HSX
 
 instance ToMessage XML where
     toContentType _ = P.pack "text/html;charset=utf-8"
@@ -34,13 +32,31 @@ instance ToMessage (Maybe XMLMetaData, XML) where
     toMessage (Nothing, xml) =
         L.fromString (renderAsHTML xml)
 
--- | Converts a @HSP XML@ to a Happstack Response.
+-- | A generic webpage template
+defaultTemplate :: (XMLGenerator m, EmbedAsChild m headers, EmbedAsChild m body) => 
+                   String   -- ^ text to use in \<title\> tag
+                -> headers  -- ^ extra headers to insert in \<head\> tag. Use @()@ if none.
+                -> body     -- ^ content to put between the \<body\> tags.
+                -> m (HSX.XML m)
+defaultTemplate title headers body =
+    unXMLGenT $
+    <html>
+     <head>
+      <title><% title %> </title>
+      <% headers %>
+     </head>
+     <body>
+      <% body %>
+     </body>
+    </html>
+
+-- | Converts a @HSP XML@ to a Happstack 'Response'.
 -- Since @HSP XML@ is the type returned by using literal HTML syntax
--- with HSP, you can wrap up your HTML as webHSP $ <html>...</html>
+-- with HSP, you can wrap up your HTML as webHSP $ \<html\>...\</html\>
 -- to use it with Happstack.
 webHSP :: (MonadIO m) => HSP XML -> m Response
 webHSP = webHSP' Nothing
 
--- | webHSP with XMLMetaData
+-- | webHSP with 'XMLMetaData'
 webHSP' :: (MonadIO m) => Maybe XMLMetaData -> HSP XML -> m Response
 webHSP' metadata hsp = toResponse `liftM` liftIO (evalHSP metadata hsp)
