874c3c7c2185515f7eaf92d1a29884d7f5aae372
jeremy <jeremy@n-heptane.com>
2011-07-27 17:55:20 +0000

    happstack-hamlet 6.1.2 - updated to hamlet 0.9

--- happstack-hamlet/Happstack/Server/Hamlet.hs
+++ happstack-hamlet/Happstack/Server/Hamlet.hs
@@ -1,28 +1,22 @@
 -- |This module provides support for using Hamlet with Happstack. Hamlet provides type-safe HTML generation. More information can be found at <http://patch-tag.com/r/mae/happstack/snapshot/current/content/pretty/happstack-hamlet/demo.hs> and <http://hackage.haskell.org/package/hamlet>.
-{-# LANGUAGE QuasiQuotes, ScopedTypeVariables, FlexibleContexts, FlexibleInstances, MultiParamTypeClasses, UndecidableInstances #-}
 module Happstack.Server.Hamlet 
     ( hamletToResponse 
     )
     where
 
-import qualified Data.ByteString.Char8      as B
-import qualified Data.ByteString.Lazy.Char8 as L
-import qualified Data.Map                   as M
-import           Data.Text                  (Text)
-import Text.Hamlet
-import Happstack.Server
-
+import Data.Text         (Text)
+import Text.Hamlet       (Hamlet)
+import Happstack.Server  (Response, toResponse)
 
 -- |turn a 'Hamlet' template into a Happstack 'Response'
+--
+-- Due to changes in Hamlet, this function is now just defined as:
+--
+-- > hamletToResponse showFn hamlet = toResponse $ hamlet showFn
+--
+-- You probably do not need to use this library at all these days.
 hamletToResponse :: (url -> [(Text, Text)] -> Text) -- ^ function to 'url' values in the template into their 'String' representation
                  -> Hamlet url  -- ^ a 'Hamlet' template
                  -> Response
 hamletToResponse showFn hamlet = 
-    let msg = renderHamlet showFn hamlet
-     in toResponse_ (B.pack "text/html; charset=UTF-8") msg
-
--- available as toResponseBS in happstack-server >= 0.5
-toResponse_ :: B.ByteString -> L.ByteString -> Response
-toResponse_ contentType message =
-    let res = Response 200 M.empty nullRsFlags message Nothing
-    in setHeaderBS (B.pack "Content-Type") contentType res
+    toResponse $ hamlet showFn
