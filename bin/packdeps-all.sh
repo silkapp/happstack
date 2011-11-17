3925f1b6f9e99dc0965c56a6f99232f59bb5ecd4
jeremy <jeremy@n-heptane.com>
2011-11-17 20:25:32 +0000

    bin: add happstack-jmacro to packdeps-all.sh

--- bin/packdeps-all.sh
+++ bin/packdeps-all.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for package in happstack-util happstack-data ixset happstack-ixset happstack-state happstack-server happstack happstack-hamlet happstack-heist happstack-hsp happstack-hstringtemplate happstack-plugins happstack-lite
+for package in happstack-util happstack-data ixset happstack-ixset happstack-state happstack-server happstack happstack-hamlet happstack-heist happstack-hsp happstack-hstringtemplate happstack-plugins happstack-jmacro happstack-lite
 do
   packdeps $package/$package.cabal
 done
